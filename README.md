ef7f2450e95620af67b41a83027981b573288310
Ed Dibb <cedibb@outlook.com>
2019-09-03 23:09:02 -0300

    refactoring

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # simple-scrollreveal
-An easy way to add scrollreveal effect to HTML elements, using pure CSS and JavaScript.
+An easy way to add scrollreveal effect to HTML elements using pure CSS and JavaScript.
 
 Demo: https://codepen.io/Dibb/pen/yRrZej
--- fade.js
+++ fade.js
@@ -1,20 +1,9 @@
-let distanceFromTop = 0.6 * window.innerHeight; // How distant the fade element should be when the fade triggers;
-                                                  //In this case, it is equals to 60% of the window height.
 
-function elementFade(element) {
-  if (element.getBoundingClientRect().top < distanceFromTop) {
-    element.classList.add('in');
-  }
-  
-  // Uncomment for elements to fade-in and out on scroll
-  //  else { element.classList.remove('in'); }
-}
-
-function scanForFadeElements() {
-  let fadeElements = document.getElementsByClassName("fade");
-  for(let i = 0; i < fadeElements.length; i++) {
-    elementFade(fadeElements[i]);
-  }
-}
-
-window.addEventListener('scroll', scanForFadeElements);
+let fadeElements = document.querySelectorAll(".fade");
+let distanceFromTop = 0.6 * window.innerHeight; /* How distant the fade element should be when the fade triggers;
+                                                   In this case, it is equals to 60% of the window height. */
+window.addEventListener('scroll', () => {
+  fadeElements.forEach((e) => {
+    (e.getBoundingClientRect().top < distanceFromTop) ? e.classList.add('in') : false
+  });
+});
