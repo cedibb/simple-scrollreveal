6711f746b253856289eb6b20f80ba7a400dbc93a
Ed Dibb <cedibb@outlook.com>
2019-09-03 23:28:09 -0300

    Refactoring

--- fade.js
+++ fade.js
@@ -1,9 +1,10 @@
 
+const DISTANCE_FROM_TOP = 0.6; // How distant the fade element should be when the fade triggers; In this case, it is equals to 60% of the window height. //
+let animationTrigger = DISTANCE_FROM_TOP * window.innerHeight; 
 let fadeElements = document.querySelectorAll(".fade");
-let distanceFromTop = 0.6 * window.innerHeight; /* How distant the fade element should be when the fade triggers;
-                                                   In this case, it is equals to 60% of the window height. */
+
 window.addEventListener('scroll', () => {
   fadeElements.forEach((e) => {
-    (e.getBoundingClientRect().top < distanceFromTop) ? e.classList.add('in') : false
+    (e.getBoundingClientRect().top < animationTrigger) ? e.classList.add('in') : false
   });
 });
