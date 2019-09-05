d797871577d7579a339088732e4779370b8c46dd
Carlos E. Dibb <cedibb@outlook.com>
2019-09-04 23:14:55 -0300

    Prettified

--- fade.js
+++ fade.js
@@ -1,10 +1,11 @@
-
 const DISTANCE_FROM_TOP = 0.6; // How distant the fade element should be when the fade triggers; In this case, it is equals to 60% of the window height. //
-let animationTrigger = DISTANCE_FROM_TOP * window.innerHeight; 
+let animationTrigger = DISTANCE_FROM_TOP * window.innerHeight;
 let fadeElements = document.querySelectorAll(".fade");
 
-window.addEventListener('scroll', () => {
-  fadeElements.forEach((e) => {
-    (e.getBoundingClientRect().top < animationTrigger) ? e.classList.add('in') : false
+window.addEventListener("scroll", () => {
+  fadeElements.forEach(e => {
+    e.getBoundingClientRect().top < animationTrigger
+      ? e.classList.add("in")
+      : false;
   });
 });
